440ff72744aab60e8367dc4aed699e5fa3e578c4
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-16 21:04:41 +0700

    Class MyButton

---
+++ Kotlin-MyCustomView/app/src/main/java/com/ahmadabuhasan/mycustomview/MyButton.kt
@@ -0,0 +1,58 @@
+package com.ahmadabuhasan.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+
+class MyButton : AppCompatButton {
+
+    private lateinit var enableBackground: Drawable
+    private lateinit var disableBackground: Drawable
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    // Metode onDraw() digunakan untuk mengcustom button ketika enable dan disable
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        // Mengubah background dari Button
+        background = if (isEnabled) enableBackground else disableBackground
+
+        // Mengubah warna text pada button
+        setTextColor(txtColor)
+
+        // Mengubah ukuran text pada button
+        textSize = 12f
+
+        // Menjadikan object pada button menjadi center
+        gravity = Gravity.CENTER
+
+        // Mengubah text pada button pada kondisi enable dan disable
+        text = if (isEnabled) "Submit" else "Isi Dulu"
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enableBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disableBackground =
+            ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+}
