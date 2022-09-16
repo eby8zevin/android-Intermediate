3f9a0e69c34c0edef9e82c75ead98002c11716b4
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-16 21:03:08 +0700

    Background Button

--- Kotlin-MyCustomView/app/src/main/java/com/ahmadabuhasan/mycustomview/MainActivity.kt
+++ Kotlin-MyCustomView/app/src/main/java/com/ahmadabuhasan/mycustomview/MainActivity.kt
@@ -2,10 +2,49 @@ package com.ahmadabuhasan.mycustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+
+        setMyButtonEnable()
+
+        myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+
+        myButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                myEditText.text,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
