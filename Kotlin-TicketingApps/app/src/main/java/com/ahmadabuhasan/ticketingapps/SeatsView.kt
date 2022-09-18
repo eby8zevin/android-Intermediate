6a35281b563ccddebf25c061e00b0904c8f933a8
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:07:49 +0700

    provide data to declare each seat

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
@@ -15,4 +15,15 @@ class SeatsView : View {
         attrs,
         defStyleAttr
     )
+
+    private val seats: ArrayList<Seat> = arrayListOf(
+        Seat(id = 1, name = "A1", isBooked = false),
+        Seat(id = 2, name = "A2", isBooked = false),
+        Seat(id = 3, name = "B1", isBooked = false),
+        Seat(id = 4, name = "A4", isBooked = false),
+        Seat(id = 5, name = "C1", isBooked = false),
+        Seat(id = 6, name = "C2", isBooked = false),
+        Seat(id = 7, name = "D1", isBooked = false),
+        Seat(id = 8, name = "D2", isBooked = false),
+    )
 }
