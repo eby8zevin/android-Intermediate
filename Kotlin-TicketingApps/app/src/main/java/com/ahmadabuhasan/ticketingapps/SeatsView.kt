b465b19623b8364f3f32f40e8e00a5db503d1ea8
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:24:16 +0700

    onClick all views in full can be suppressed

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
@@ -1,11 +1,14 @@
 package com.ahmadabuhasan.ticketingapps
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_DOWN
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
@@ -129,5 +132,33 @@ class SeatsView : View {
 
         // Mengembalikan ke pengaturan sebelumnya
         canvas?.restore()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val halfOfHeight = height / 2
+        val halfOfWidth = width / 2
+
+        val widthColumnOne = (halfOfWidth - 300F)..(halfOfWidth - 100F)
+        val widthColumnTwo = (halfOfWidth + 100F)..(halfOfWidth + 300F)
+
+        val heightRowOne = (halfOfHeight - 600F)..(halfOfHeight - 400F)
+        val heightRowTwo = (halfOfHeight - 300F)..(halfOfHeight - 100F)
+        val heightRowTree = (halfOfHeight + 0F)..(halfOfHeight + 200F)
+        val heightRowFour = (halfOfHeight + 300F)..(halfOfHeight + 500F)
+
+        if (event?.action == ACTION_DOWN) {
+            when {
+                event.x in widthColumnOne && event.y in heightRowOne -> booking(0)
+                event.x in widthColumnTwo && event.y in heightRowOne -> booking(1)
+                event.x in widthColumnOne && event.y in heightRowTwo -> booking(2)
+                event.x in widthColumnTwo && event.y in heightRowTwo -> booking(3)
+                event.x in widthColumnOne && event.y in heightRowTree -> booking(4)
+                event.x in widthColumnTwo && event.y in heightRowTree -> booking(5)
+                event.x in widthColumnOne && event.y in heightRowFour -> booking(6)
+                event.x in widthColumnTwo && event.y in heightRowFour -> booking(7)
+            }
+        }
+        return true
     }
 }
