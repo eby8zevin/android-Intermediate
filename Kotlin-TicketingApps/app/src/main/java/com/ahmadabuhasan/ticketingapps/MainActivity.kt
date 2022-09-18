204d51b6ec13dbc245ab2861786f3a4ba92dc2fa
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:42:51 +0700

    zoom in and remove toolbar

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
         val seatView = findViewById<SeatsView>(R.id.seatsView)
