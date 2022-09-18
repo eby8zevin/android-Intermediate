a59185a7d1b04281fa61171ea8bc802d98302570
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:09:03 +0700

    call the onMeasure method and declare the values of x and y

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/SeatsView.kt
@@ -26,4 +26,29 @@ class SeatsView : View {
         Seat(id = 7, name = "D1", isBooked = false),
         Seat(id = 8, name = "D2", isBooked = false),
     )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
+        val height = getDefaultSize(suggestedMinimumHeight, heightMeasureSpec)
+
+        val halfOfHeight = height / 2
+        val halfOfWidth = width / 2
+        var value = -600F
+
+        for (i in 0..7) {
+            if (i.mod(2) == 0) {
+                seats[i].apply {
+                    x = halfOfWidth - 300F
+                    y = halfOfHeight + value
+                }
+            } else {
+                seats[i].apply {
+                    x = halfOfWidth + 100F
+                    y = halfOfHeight + value
+                }
+                value += 300F
+            }
+        }
+    }
 }
