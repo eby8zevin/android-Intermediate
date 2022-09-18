a79ad0410e06bd678b8bf2b97234a573f2a1ec01
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 23:34:07 +0700

    Create a Canvas with the help of Bitmap

--- Kotlin-LikesApp/app/src/main/java/com/ahmadabuhasan/likesapp/MainActivity.kt
+++ Kotlin-LikesApp/app/src/main/java/com/ahmadabuhasan/likesapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.ahmadabuhasan.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ahmadabuhasan.likesapp.databinding.ActivityMainBinding
@@ -8,9 +11,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imageView.setImageBitmap(mBitmap)
     }
 }
