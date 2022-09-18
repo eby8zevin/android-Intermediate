4aa328b193377575cd4a3c260bc2ee936dd885a2
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:42:27 +0700

    connecting SeatsView and Button with Activity

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ahmadabuhasan.ticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
     }
 }
