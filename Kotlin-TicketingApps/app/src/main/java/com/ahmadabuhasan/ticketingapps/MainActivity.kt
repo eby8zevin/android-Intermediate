f42b5a5ff310f075b74b299254b220a125cd07fe
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-18 13:45:03 +0700

    added onClick function on Button to provide action when seat data has been selected

--- Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
+++ Kotlin-TicketingApps/app/src/main/java/com/ahmadabuhasan/ticketingapps/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         val seatView = findViewById<SeatsView>(R.id.seatsView)
         val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
 }
