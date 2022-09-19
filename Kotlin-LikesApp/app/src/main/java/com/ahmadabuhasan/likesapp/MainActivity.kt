0e7ac6bcdc7b9bd916a80588f80ee220b7066114
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-19 18:38:21 +0700

    Add method showText

--- Kotlin-LikesApp/app/src/main/java/com/ahmadabuhasan/likesapp/MainActivity.kt
+++ Kotlin-LikesApp/app/src/main/java/com/ahmadabuhasan/likesapp/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.ahmadabuhasan.likesapp
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.res.ResourcesCompat
@@ -16,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
     private val mCanvas = Canvas(mBitmap)
     private val mPaint = Paint()
+
+    private val message = "Apakah kamu suka bermain?"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -103,5 +102,19 @@ class MainActivity : AppCompatActivity() {
                 mCanvas.drawArc(mouth, 0F, -180F, false, mPaint)
             }
         }
+    }
+
+    private fun showText() {
+        val mPaintText = Paint(Paint.FAKE_BOLD_TEXT_FLAG).apply {
+            textSize = 50F
+            color = ResourcesCompat.getColor(resources, R.color.black, null)
+        }
+
+        val mBounds = Rect()
+        mPaintText.getTextBounds(message, 0, message.length, mBounds)
+
+        val x: Float = halfOfWidth - mBounds.centerX()
+        val y = 50F
+        mCanvas.drawText(message, x, y, mPaintText)
     }
 }
