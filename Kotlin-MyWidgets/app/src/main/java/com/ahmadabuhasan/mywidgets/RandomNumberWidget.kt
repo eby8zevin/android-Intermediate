18a0d34330816b7aa9dc75f605013bb7d551d372
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-19 20:00:38 +0700

    implement method onReceive()

--- Kotlin-MyWidgets/app/src/main/java/com/ahmadabuhasan/mywidgets/RandomNumberWidget.kt
+++ Kotlin-MyWidgets/app/src/main/java/com/ahmadabuhasan/mywidgets/RandomNumberWidget.kt
@@ -1,8 +1,11 @@
 package com.ahmadabuhasan.mywidgets
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 
 /**
@@ -20,12 +23,47 @@ class RandomNumberWidget : AppWidgetProvider() {
         }
     }
 
+    companion object {
+        private const val WIDGET_CLICK = "android.appwidget.action.APPWIDGET_UPDATE"
+        private const val WIDGET_ID_EXTRA = "widget_id_extra"
+    }
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (WIDGET_CLICK == intent.action) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val views = RemoteViews(context.packageName, R.layout.random_number_widget)
+            val lastUpdate = "Random: " + NumberGenerator.generate(100)
+            val appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0)
+            views.setTextViewText(R.id.appwidget_text, lastUpdate)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    private fun getPendingSelfIntent(
+        context: Context,
+        appWidgetId: Int,
+        action: String
+    ): PendingIntent {
+        val intent = Intent(context, RandomNumberWidget::class.java)
+        intent.action = action
+        intent.putExtra(WIDGET_ID_EXTRA, appWidgetId)
+        return PendingIntent.getBroadcast(
+            context, appWidgetId, intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            } else {
+                0
+            }
+        )
     }
 }
 
