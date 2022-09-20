50840742578b927e6fc089df45e29a8bf8772cca
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-20 19:39:03 +0700

    Change method onUpdate

--- Kotlin-MyStackWidget/app/src/main/java/com/ahmadabuhasan/mystackwidget/ImageBannerWidget.kt
+++ Kotlin-MyStackWidget/app/src/main/java/com/ahmadabuhasan/mystackwidget/ImageBannerWidget.kt
@@ -1,44 +1,50 @@
 package com.ahmadabuhasan.mystackwidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
+import androidx.core.net.toUri
 
 /**
  * Implementation of App Widget functionality.
  */
 class ImageBannerWidget : AppWidgetProvider() {
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+
+    companion object {
+        private const val TOAST_ACTION = "com.ahmadabuhasan.mystackwidget.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.ahmadabuhasan.mystackwidget.EXTRA_ITEM"
+
+        //pindahkan fungsi ini ke companion object, karena kita akan memanggil fungsi ini dari luar kelas
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val views = RemoteViews(context.packageName, R.layout.image_banner_widget)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            val toastIntent = Intent(context, ImageBannerWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context, 0, toastIntent,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+                else 0
+            )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.image_banner_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
