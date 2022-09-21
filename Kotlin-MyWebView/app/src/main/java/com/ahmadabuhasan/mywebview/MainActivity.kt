7db348c954bbe399504d0382c909005cc7a5143c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-21 18:19:38 +0700

    Add loadUrl WebView

--- Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
+++ Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ahmadabuhasan.mywebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.loadUrl("https://www.dicoding.com")
     }
 }
