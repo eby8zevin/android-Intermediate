5eadadb859186b2c3b008cddd7d28ba74e8b266b
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-21 18:24:18 +0700

    Config WebChromeClient

--- Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
+++ Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ahmadabuhasan.mywebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
@@ -16,9 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
-                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG)
+                    .show()
             }
         }
+
+        webView.webChromeClient = WebChromeClient()
 
         webView.loadUrl("https://www.dicoding.com")
     }
