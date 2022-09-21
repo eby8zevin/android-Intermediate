4a4a49c6ceec294902ac186a26a7cc0fa88f8d98
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-21 18:28:14 +0700

    Change WebChromeClient & WebViewClient

--- Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
+++ Kotlin-MyWebView/app/src/main/java/com/ahmadabuhasan/mywebview/MainActivity.kt
@@ -17,12 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
-                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG)
-                    .show()
+                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
             }
         }
 
-        webView.webChromeClient = WebChromeClient()
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: android.webkit.JsResult
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                result.confirm()
+                return true
+            }
+        }
 
         webView.loadUrl("https://www.dicoding.com")
     }
