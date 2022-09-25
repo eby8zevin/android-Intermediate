5cb26e59639834b72580d9fb433285e42d48e4dc
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-25 18:55:40 +0700

    Add animation to image

--- Kotlin-LatihanPropertyAnimationStarter/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/welcome/WelcomeActivity.kt
+++ Kotlin-LatihanPropertyAnimationStarter/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/welcome/WelcomeActivity.kt
@@ -1,8 +1,10 @@
 package com.dicoding.picodiploma.loginwithanimation.view.welcome
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -24,7 +26,11 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun playAnimation() {
-
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
     }
 
     private fun setupView() {
