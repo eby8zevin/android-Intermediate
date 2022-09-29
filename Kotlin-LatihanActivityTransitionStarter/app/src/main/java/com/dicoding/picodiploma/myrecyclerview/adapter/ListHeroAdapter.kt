d1d21c3a56898ee044ddd51b2bbb9691c351c1b6
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-09-29 18:52:37 +0700

    Add ActivityOptionsCompat

--- Kotlin-LatihanActivityTransitionStarter/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
+++ Kotlin-LatihanActivityTransitionStarter/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -48,7 +48,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
             itemView.setOnClickListener {
                 val intent = Intent(itemView.context, HeroActivity::class.java)
                 intent.putExtra("Hero", hero)
-                itemView.context.startActivity(intent)
+                itemView.context.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity).toBundle())
             }
         }
     }
