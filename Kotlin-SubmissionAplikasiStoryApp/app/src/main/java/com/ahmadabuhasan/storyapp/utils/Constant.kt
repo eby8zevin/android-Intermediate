2c220b0eeae270f72211ed7fe95e4f07dbd812a0
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-11-15 05:14:09 +0700

    Global Variable

---
+++ Kotlin-SubmissionAplikasiStoryApp/app/src/main/java/com/ahmadabuhasan/storyapp/utils/Constant.kt
@@ -0,0 +1,14 @@
+package com.ahmadabuhasan.storyapp.utils
+
+object Constant {
+
+    const val PREFS_NAME = "story.app_pref"
+
+    const val KEY_IS_LOGIN = "key_login"
+    const val KEY_TOKEN = "key_token"
+    const val KEY_USER_ID = "key_user_id"
+    const val KEY_NAME = "key_name"
+    const val KEY_EMAIL = "key_email"
+
+    const val BUNDLE_STORY = "bundle_story"
+}
