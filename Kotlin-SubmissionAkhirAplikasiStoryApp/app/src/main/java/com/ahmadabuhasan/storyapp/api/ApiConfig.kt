ce7259a503428db96af3d59eeb88f8143fd27daa
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-11-16 01:08:40 +0700

    member statis & pesan log logging interceptor - Notes from Reviewers

--- Kotlin-SubmissionAkhirAplikasiStoryApp/app/src/main/java/com/ahmadabuhasan/storyapp/api/ApiConfig.kt
+++ Kotlin-SubmissionAkhirAplikasiStoryApp/app/src/main/java/com/ahmadabuhasan/storyapp/api/ApiConfig.kt
@@ -1,30 +1,32 @@
 package com.ahmadabuhasan.storyapp.api
 
+import androidx.viewbinding.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class ApiConfig {
+object ApiConfig {
+    fun getApiService(): ApiService {
 
-    companion object {
-        fun getApiService(): ApiService {
+        val client = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG) {
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .connectTimeout(150, TimeUnit.SECONDS)
-                .readTimeout(150, TimeUnit.SECONDS)
-                .build()
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://story-api.dicoding.dev/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-            return retrofit.create(ApiService::class.java)
+            client.addInterceptor(loggingInterceptor)
+            client.connectTimeout(150, TimeUnit.SECONDS)
+            client.readTimeout(150, TimeUnit.SECONDS)
         }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://story-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client.build())
+            .build()
+        return retrofit.create(ApiService::class.java)
+
     }
 }
