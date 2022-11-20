8fdf52bc4971d383eada688c9ebd004c064d56c6
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-11-21 00:56:29 +0700

    View Model Factory

---
+++ Kotlin-SubmissionAkhirAplikasiStoryApp/app/src/main/java/com/ahmadabuhasan/storyapp/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,30 @@
+package com.ahmadabuhasan.storyapp.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ahmadabuhasan.storyapp.data.StoryRepository
+import com.ahmadabuhasan.storyapp.di.Injection
+
+class ViewModelFactory(private val repository: StoryRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(StoryViewModel::class.java)) {
+            return StoryViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+        fun getInstance(context: Context): ViewModelFactory {
+            return instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository(context))
+            }.also { instance = it }
+        }
+    }
+}
